ccb33b512fb7ab5bcd0c017e696839fc5a71909f
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-27 16:01:23 +0200

    + 04.Distribution-of-last-two-digits.R

---
+++ 07.Explarotary-Data-Analysis/03.Variations/04.Exercises/02.Explore-price-distribution/04.Distribution-of-last-two-digits.R
@@ -0,0 +1,10 @@
+X11()
+library(tidyverse)
+
+diamonds                                   %>%
+  mutate(ending = price %% 100)            %>%
+  ggplot(aes(x = ending))         +
+     geom_histogram(binwidth = 1, center = 0)
+
+cat ("Press enter...")
+readLines("stdin", n = 1)
