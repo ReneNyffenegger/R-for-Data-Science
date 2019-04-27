f2df2da81e8a17c9c80ad850f40b7e839960038d
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-27 09:13:30 +0200

    more comment

--- 07.Explarotary-Data-Analysis/03.Variations/03.Unusual-values/01.outliers.R
+++ 07.Explarotary-Data-Analysis/03.Variations/03.Unusual-values/01.outliers.R
@@ -3,6 +3,8 @@ library(ggplot2)
   
 #
 #   Plottting the distribution of the y variable.
+#  (The y variable measures one of the three dimensions
+#   of these diamonds, in mm.)
 #
 #   The only evidence of outliers is the unusually
 #   wide limits on the x-axis.
