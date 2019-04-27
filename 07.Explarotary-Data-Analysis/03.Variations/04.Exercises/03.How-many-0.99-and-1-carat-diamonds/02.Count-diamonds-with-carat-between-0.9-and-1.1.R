5aa75faa958901bb6f9bb2877168e436a910be2e
René Nyffenegger <mail@renenyffenegger.ch>
2019-04-27 16:19:10 +0200

    Use between

--- 07.Explarotary-Data-Analysis/03.Variations/04.Exercises/03.How-many-0.99-and-1-carat-diamonds/02.Count-diamonds-with-carat-between-0.9-and-1.1.R
+++ 07.Explarotary-Data-Analysis/03.Variations/04.Exercises/03.How-many-0.99-and-1-carat-diamonds/02.Count-diamonds-with-carat-between-0.9-and-1.1.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 diamonds                              %>%
-  filter(carat >= 0.90, carat <= 1.1) %>%
+# filter(carat >= 0.90, carat <= 1.1) %>%
+  filter(between(carat, 0.9, 1.1)   ) %>% # use filter in combination with between
   count (carat )                      %>%
   print (n = 21) # Print 21 records (entire record set)
